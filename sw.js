3809c961ea46551ea9a726ee17611e32b97eb7a2
zznnll588546-wq <zznnll588546@gmail.com>
2025-12-28 21:01:46 +0800

    7.3版本

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'fgo-daily-v4.2';
+const urlsToCache = [
+  './',
+  './index.html'
+];
+
+// 安装
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// 拦截请求
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// 清理旧缓存
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
